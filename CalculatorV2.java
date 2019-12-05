38bc0a16eac5d443c0e12f82e88f44a1d60a6d7b
yogeshjain96 <yogeshjainyugi@gmail.com>
2019-12-05 03:17:46 -0800

    simple calc

---
+++ CalculatorV2.java
@@ -0,0 +1,13 @@
+class Calculator{
+	public static void main(){
+		System.out.println("Hello from PRN-0227");
+		System.out.println("Simple Java Calculator");
+		boolean exit=false;
+		int n1=5;
+		int n2=9;
+		System.out.println("Addition of"+n1+" & "+n2+" is "+(n1+n2));
+		System.out.println("Diffrence of"+n1+" & "+n2+" is "+(n1-n2));
+		System.out.println("Division of"+n1+" & "+n2+" is "+(n1/n2));
+		System.out.println("Multiplcation of"+n1+" & "+n2+" is "+(n1*n2));
+	}
+}
